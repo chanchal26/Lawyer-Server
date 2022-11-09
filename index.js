8aa05fb1f98ee28844549625f51af32358a1af63
chanchal26 <chanchal.mch2613@gmail.com>
2022-11-09 15:37:26 +0600

    dot env successfully added

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://lawyer-db:PLlAcOXxetPxrRe@cluster0.v73oziy.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v73oziy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
@@ -54,14 +55,54 @@ app.post('/reviews', async (req, res) => {
 });
 
 
+app.post('/services', async (req, res) => {
+    try {
+        const result = await Services.insertOne(req.body);
+        if (result.insertedId) {
+            res.send({
+                success: true,
+                message: `Successfully created the review with id ${result.insertedId}`
+            })
+        } else {
+            res.send({
+                success: false,
+                message: "Couldn't create the review"
+            })
+        }
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+});
+
+
 app.get('/reviews', async (req, res) => {
     try {
         const cursor = await Reviews.find({});
-        const reviews = await cursor.toArray();
+        const reviews = (await cursor.toArray()).reverse();
         res.send({
             success: true,
             message: `Successfully got the data`,
             data: reviews
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+});
+
+app.get('/services/home', async (req, res) => {
+    try {
+        const cursor = await Services.find({}).limit(3);
+        const services = await cursor.toArray();
+        res.send({
+            success: true,
+            message: `Successfully got the data`,
+            data: services
         })
     } catch (error) {
         res.send({
